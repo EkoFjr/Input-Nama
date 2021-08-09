81fd7c4e47903764f295cc5e437bd8853451d6e3
EKO <eofrpa@gmail.com>
2021-08-09 14:55:19 +0700

    menambahkan sistem kerja

--- app/src/main/java/com/example/inputnama/MainActivity.java
+++ app/src/main/java/com/example/inputnama/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.inputnama;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    EditText inputname;
+    TextView hasil;
+    Button tampil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputname = (EditText) findViewById(R.id.inputnama);
+        hasil = (TextView) findViewById(R.id.tampilan);
+        tampil = (Button) findViewById(R.id.button);
+
+        tampil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                hasil.setText("Nama Anda : "+inputname.getText());
+            }
+        });
+
     }
+
+
 }

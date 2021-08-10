f8354e25cfed086f2640839c02805b28a5ca00d5
EKO <eofrpa@gmail.com>
2021-08-10 16:58:14 +0700

    fitur baru

--- app/src/main/java/com/example/inputnama/MainActivity.java
+++ app/src/main/java/com/example/inputnama/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     EditText inputname;
     TextView hasil;
-    Button tampil;
+    Button tampil,hapus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +22,23 @@ public class MainActivity extends AppCompatActivity {
         inputname = (EditText) findViewById(R.id.inputnama);
         hasil = (TextView) findViewById(R.id.tampilan);
         tampil = (Button) findViewById(R.id.button);
+        hapus = (Button) findViewById(R.id.button2);
 
         tampil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
                 hasil.setText("Nama Anda : "+inputname.getText());
+            }
+        });
+
+        hapus.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                inputname.setText("");
+                hasil.setText("");
             }
         });
 
